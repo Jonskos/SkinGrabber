e818fe5ab7c873b91413a535dc4dcc8220b7128e
Jonskos <joonas.melvasalo@protonmail.com>
2025-03-13 22:28:35 +0200

    Add grabbing skins of players in lobby

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Bonk Skin Grabber
-// @version      1.0.0
+// @version      1.0.1
 // @description
 // @match        https://bonk.io/gameframe-release.html
 // @run-at       document-idle
@@ -24,6 +24,7 @@ for (
   icons[i] = document.getElementById(`skinmanager_skin${i + 1}_icon`);
 }
 //const icons = Array.from(document.getElementById("skinmanager_allicons").children).map((element) => element.children[0]);
+window.inLobby = false;
 
 function injector(src) {
   let newSrc = src;
@@ -32,6 +33,7 @@ function injector(src) {
   const avatarRegex =
     /;(.{1,3}\[\d{1,3}\])=class(?:(?!}};).)*?makeSafe\(\) .*?}};/;
   const avatarMatch = src.match(avatarRegex);
+  if (avatarMatch === null) throw "Avatar class not found!";
   newSrc = newSrc.replace(
     avatarMatch[0],
     avatarMatch[0] + `window.Avatar = ${avatarMatch[1]};`
@@ -44,25 +46,29 @@ function injector(src) {
 
   const setPlayerArrayRegex = /(?<=})setPlayerArray.*?}/;
   const setPlayerArrayMatch = rendererMatch[0].match(setPlayerArrayRegex);
-  //set window.playerarray to arguments whenever setplayerarray is called (note. on all instances, so conflicts theoretically possible)
+  //set window.currentPlayerArray to arguments whenever setplayerarray is called (note. on all instances, so conflicts theoretically possible)
   //using a arrow function cuz i dont feel like naming a variable setPlayerArrayArray
-  const setPlayerArray = ((src) => {
-    let array = src.split(";");
-    array.splice(array.length - 1, 0, "window.playerArray = [arguments][0][0]");
+  const setPlayerArray = (() => {
+    let array = setPlayerArrayMatch[0].split(";");
+    array.splice(
+      array.length - 1,
+      0,
+      "window.rendererPlayerArray = [arguments][0][0]"
+    );
     return array.join(";");
-  })(setPlayerArrayMatch[0]);
+  })();
   const renderer = rendererMatch[0].replace(
     setPlayerArrayMatch[0],
     setPlayerArray
   );
+  if (rendererMatch === null) throw "Renderer class not found!";
   newSrc = newSrc.replace(rendererMatch[0], renderer);
-  //need to replace magic strings like function names later
-  // const saveAvatarRegex = /;function (X7_)\(.*?\){.*?;}}/;
 
   const saveAvatarRegex = /;function (\w{1,3})\((?:(?!;}}).)*?newavatar:.*?;}}/;
   const saveAvatarMatch = src.match(saveAvatarRegex);
-  //rename func then remove close window function call
+  if (saveAvatarMatch === null) throw "Save avatar function not found!";
   const saveAvatarFuncName = "saveAvatarBg";
+  //rename func then remove close window function call
   const saveAvatarFunc = saveAvatarMatch[0]
     .replace(saveAvatarMatch[1], saveAvatarFuncName)
     .replace(/;\w{1,3}\(\)/, "");
@@ -82,6 +88,50 @@ function injector(src) {
     updateIconMatch[0] + `window.updateIcon = ${updateIconMatch[1]};`
   );
 
+  /* const handlePlayerJoinRegex =
+    /;function \w{1,3}\((?:(?!;}}).)*?;((.{1,3}\[\d{1,3}\])\[.{1,3}\[0\]\[0\]\]={userName:.*?\};).*?;}}.{1,3}\[\d{1,3}\]=document/;
+  const handlePlayerJoinMatch = src.match(handlePlayerJoinRegex);
+  const handlePlayerJoin = handlePlayerJoinMatch[0].replace(
+    handlePlayerJoinMatch[1],
+    handlePlayerJoinMatch[1] +
+      `window.lobbyPlayerArray=${handlePlayerJoinMatch[2]};`
+  );
+  console.log(handlePlayerJoin);
+  newSrc = newSrc.replace(handlePlayerJoinMatch[0], handlePlayerJoin); */
+
+  const lobbyPlayerArrayRegex =
+    /;function \w{1,3}\((?:(?!;}}).)*?;(.{1,3}\[\d{1,3}\])\[.{1,3}\[0\]\[0\]\]={userName:.*?\};.*?;}}.{1,3}\[\d{1,3}\]=document/;
+  const lobbyPlayerArrayMatch = src.match(lobbyPlayerArrayRegex);
+
+  /*   const lobbyPlayerArrayInitRegex = new RegExp(lobbyPlayerArrayMatch[1] + "=\[\];");
+  const lobbyPlayerArrayInitMatch = src.match(lobbyPlayerArrayInitRegex);
+  console.log(lobbyPlayerArrayInitMatch); */
+  // newSrc = newSrc.replace(lobbyPlayerArrayInitMatch[0], lobbyPlayerArrayInitMatch[0] + "window.lobbyPlayerArray=" + lobbyPlayerArrayMatch[1]);
+
+  newSrc = newSrc.replace(
+    `${lobbyPlayerArrayMatch[1]}=[];`,
+    `${lobbyPlayerArrayMatch[1]}=[];window.lobbyPlayerArray=${lobbyPlayerArrayMatch[1]};`
+  );
+
+  const networkEngineRegex =
+    /;function \w{1,3}\((?:.{1,3},?){3}\){var .{1,3}=\[arguments\];.{1,3}\[2\]=.{1,5};.{1,3}\[1\]=1(?:(?!;};}).)*?;};}/;
+  const networkEngineMatch = src.match(networkEngineRegex);
+
+  const networkEngine = (() => {
+    let array = networkEngineMatch[0].split(";");
+    array.splice(3, 0, "window.inLobby = true");
+    array.splice(
+      array.length - 2, //technically this works, as the destructor is the last function, but it might be unreliable if they change the order of the functions
+      0,
+      "window.inLobby = false"
+    );
+    return array.join(";");
+  })();
+
+  console.log(networkEngine);
+
+  newSrc = newSrc.replace(networkEngineMatch[0], networkEngine);
+
   if (src === newSrc) throw "Injection failed!";
   console.log(injectorName + " injector run");
   return newSrc;
@@ -98,11 +148,17 @@ window.bonkCodeInjectors.push((bonkCode) => {
 });
 
 function grabAvatar(username, openLink = true) {
-  if (username === null) throw new Error("invalid username");
-  const player = window.playerArray
-    .filter((player) => player !== null)
-    .find((player) => player.userName == username);
-  if (player === undefined) throw new Error("could not find player");
+  //currently only works for players in game / on the renderer
+  if (username === null) throw "Invalid username";
+
+  const player = inLobby
+    ? window.rendererPlayerArray
+        .filter((player) => player !== null)
+        .find((player) => player.userName == username)
+    : window.lobbyPlayerArray
+        .filter((player) => player !== null)
+        .find((player) => player.userName == username);
+  if (player === undefined) throw "Could not find player";
 
   const avatar = new Avatar();
   avatar.fromObject(player.avatar);
